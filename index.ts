5b4c4613f42a8ab7891c37d11c553e7a807f5f8c
haruharuharuby <cocktail.house@gmail.com>
2019-12-03 15:48:47 +0100

    add cloudfront mock

--- index.ts
+++ index.ts
@@ -82,7 +82,7 @@ export const mockDynamo = {
             .mockImplementationOnce(() => { return mockAsyncIterator(result2) })
     },
 
-    get: function (result: any) : jest.SpyInstance {
+    get: function (result: any): jest.SpyInstance {
         return jest.spyOn(DataMapper.prototype, 'get')
             .mockImplementationOnce(async () => result)
     },
@@ -93,7 +93,14 @@ export const mockDynamo = {
             .mockImplementationOnce(async () => result2)
     },
 
-    put: function (result: any) : jest.SpyInstance {
+    getThirth: function (result1: any, result2: any, result3: any): jest.SpyInstance {
+        return jest.spyOn(DataMapper.prototype, 'get')
+            .mockImplementationOnce(async () => result1)
+            .mockImplementationOnce(async () => result2)
+            .mockImplementationOnce(async () => result3)
+    },
+
+    put: function (result: any): jest.SpyInstance {
         return jest.spyOn(DataMapper.prototype, 'put')
             .mockImplementationOnce(async () => result)
     },
@@ -101,6 +108,12 @@ export const mockDynamo = {
     update: function (result: any): jest.SpyInstance {
         return jest.spyOn(DataMapper.prototype, 'update')
             .mockImplementationOnce(async () => result)
+    },
+
+    updateTwice: function (result1: any, result2: any): jest.SpyInstance {
+        return jest.spyOn(DataMapper.prototype, 'update')
+            .mockImplementationOnce(async () => result1)
+            .mockImplementationOnce(async () => result2)
     },
 
     delete: function (result: any): jest.SpyInstance {
@@ -113,7 +126,11 @@ export const mockDynamo = {
             .mockImplementationOnce(() => { return mockAsyncIterator(result) })
     },
 
-    throw: function (): jest.SpyInstance {
+    queryWithThrow: function (): jest.SpyInstance {
+        return jest.spyOn(DataMapper.prototype, 'query')
+            .mockImplementationOnce(() => { throw new Error('mock exception') })
+    },
+    getWIthThrow: function (): jest.SpyInstance {
         return jest.spyOn(DataMapper.prototype, 'query')
             .mockImplementationOnce(() => { throw new Error('mock exception') })
     }
@@ -136,9 +153,55 @@ export const mockLambda = {
 }
 
 export const mockS3 = {
-    getSignedUrl: function(url: string): jest.SpyInstance {
+    headObject: function (meta: {}): jest.SpyInstance {
+        // @ts-ignore
+        return jest.spyOn(AWS.S3.services['2006-03-01'].prototype, 'headObject').mockImplementation(() => {
+            return {
+                promise: () => Promise.resolve(meta)
+            }
+        })
+    },
+    headObjectRejection: function (exception: {}): jest.SpyInstance {
+        // @ts-ignore
+        return jest.spyOn(AWS.S3.services['2006-03-01'].prototype, 'headObject').mockImplementation(() => {
+            return {
+                promise: () => Promise.reject(exception)
+            }
+        })
+    },
+
+    getSignedUrl: function (url: string): jest.SpyInstance {
         return jest.spyOn(AWS.S3.prototype, 'getSignedUrl').mockImplementation(() => {
             return url
+        })
+    }
+}
+
+export const mockCloudFront = {
+    getDistribution: function (result: {}): jest.SpyInstance {
+        // @ts-ignore
+        return jest.spyOn(AWS.CloudFront.services['2019-03-26'].prototype, 'getDistribution').mockImplementation(() => {
+            return {
+                promise: () => Promise.resolve(result)
+            }
+        })
+    },
+
+    getDistributionConfig: function (result: {}): jest.SpyInstance {
+        // @ts-ignore
+        return jest.spyOn(AWS.CloudFront.services['2019-03-26'].prototype, 'getDistributionConfig').mockImplementation(() => {
+            return {
+                promise: () => Promise.resolve(result)
+            }
+        })
+    },
+
+    updateDistribution: function (result: {}): jest.SpyInstance {
+        // @ts-ignore
+        return jest.spyOn(AWS.CloudFront.services['2019-03-26'].prototype, 'updateDistribution').mockImplementation(() => {
+            return {
+                promise: () => Promise.resolve(result)
+            }
         })
     }
 }
